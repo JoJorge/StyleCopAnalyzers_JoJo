22d8362c91b59d9abb1e0856034cb3e6f6a13f4c
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-17 20:20:09 -0600

    Update SA1508 tests for line pragma

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1508CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1508CSharp10UnitTests.cs
@@ -3,9 +3,38 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp9.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1508ClosingBracesMustNotBePrecededByBlankLine,
+        StyleCop.Analyzers.LayoutRules.SA1508CodeFixProvider>;
 
     public partial class SA1508CSharp10UnitTests : SA1508CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3992, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3992")]
+        public async Task TestMappedLineDirectiveBeforeClosingBraceAsync()
+        {
+            var testCode = @"class TestClass
+{
+    private void DoWork()
+    {
+    }
+
+    public void Test()
+    {
+        if (true)
+        {
+            DoWork();
+#line (15,1)-(15,1) ""Remapped.cs""
+        }
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
