1f83c2c854d3539fc05f6ce0c3b57be73d220e02
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-17 20:19:46 -0600

    Update SA1507 tests for line pragma

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1507CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1507CSharp10UnitTests.cs
@@ -3,9 +3,31 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp9.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1507CodeMustNotContainMultipleBlankLinesInARow,
+        StyleCop.Analyzers.LayoutRules.SA1507CodeFixProvider>;
 
     public partial class SA1507CSharp10UnitTests : SA1507CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3992, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3992")]
+        public async Task TestConsecutiveMappedLineDirectivesDoNotCountAsBlankLinesAsync()
+        {
+            var testCode = @"class TestClass
+{
+#line (5,1)-(5,1) ""Remapped.cs""
+#line default
+    void Test()
+    {
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
