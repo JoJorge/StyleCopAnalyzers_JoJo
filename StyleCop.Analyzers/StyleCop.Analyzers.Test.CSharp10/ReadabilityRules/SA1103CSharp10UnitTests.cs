b88a0520634b621d117a648de422a345ccc47d17
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-17 20:21:38 -0600

    Update SA1103 tests for line pragma

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/ReadabilityRules/SA1103CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/ReadabilityRules/SA1103CSharp10UnitTests.cs
@@ -3,9 +3,34 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.ReadabilityRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp9.ReadabilityRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.ReadabilityRules.SA110xQueryClauses,
+        StyleCop.Analyzers.ReadabilityRules.SA1103CodeFixProvider>;
 
     public partial class SA1103CSharp10UnitTests : SA1103CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3992, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3992")]
+        public async Task TestMappedLineDirectiveBetweenClausesAsync()
+        {
+            var testCode = @"using System.Linq;
+public class TestClass
+{
+    public void Test()
+    {
+        var query =
+            from value in new int[0]
+#line (10,1)-(10,1) ""Remapped.cs""
+            select value;
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
