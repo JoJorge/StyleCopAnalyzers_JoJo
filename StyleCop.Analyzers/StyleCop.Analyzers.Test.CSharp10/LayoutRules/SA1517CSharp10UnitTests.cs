16598db711240ced690ebec16e1868e2d2483912
Sam Harwell <sam@tunnelvisionlabs.com>
2025-12-17 20:20:43 -0600

    Update SA1517 tests for line pragma

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1517CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/LayoutRules/SA1517CSharp10UnitTests.cs
@@ -3,9 +3,29 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.LayoutRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp9.LayoutRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.LayoutRules.SA1517CodeMustNotContainBlankLinesAtStartOfFile,
+        StyleCop.Analyzers.LayoutRules.SA1517CodeFixProvider>;
 
     public partial class SA1517CSharp10UnitTests : SA1517CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3992, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3992")]
+        public async Task TestMappedLineDirectiveAtFileStartDoesNotTriggerAsync()
+        {
+            var testCode = @"#line (1,1)-(1,1) ""Remapped.cs""
+
+class TestClass
+{
+}
+#line default";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
