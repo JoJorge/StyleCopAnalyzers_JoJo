9ebe0f278905c94c694024ee4f7761fd685b0062
Sam Harwell <sam@tunnelvisionlabs.com>
2026-01-06 10:47:01 -0600

    Add tests for mixed deconstruction in C# 10
    Closes #3990

--- StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/NamingRules/SA1316CSharp10UnitTests.cs
+++ StyleCop.Analyzers/StyleCop.Analyzers.Test.CSharp10/NamingRules/SA1316CSharp10UnitTests.cs
@@ -3,9 +3,32 @@
 
 namespace StyleCop.Analyzers.Test.CSharp10.NamingRules
 {
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Testing;
     using StyleCop.Analyzers.Test.CSharp9.NamingRules;
+    using Xunit;
+    using static StyleCop.Analyzers.Test.Verifiers.StyleCopCodeFixVerifier<
+        StyleCop.Analyzers.NamingRules.SA1316TupleElementNamesShouldUseCorrectCasing,
+        StyleCop.Analyzers.NamingRules.SA1316CodeFixProvider>;
 
     public partial class SA1316CSharp10UnitTests : SA1316CSharp9UnitTests
     {
+        [Fact]
+        [WorkItem(3990, "https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3990")]
+        public async Task TestMixedDeconstructionIsIgnoredAsync()
+        {
+            var testCode = @"public class TestClass
+{
+    public void TestMethod()
+    {
+        var tuple = (ValueA: 1, ValueB: 2);
+        int existing = 0;
+        (existing, int ValueC) = tuple;
+    }
+}";
+
+            await VerifyCSharpDiagnosticAsync(testCode, DiagnosticResult.EmptyDiagnosticResults, CancellationToken.None).ConfigureAwait(false);
+        }
     }
 }
